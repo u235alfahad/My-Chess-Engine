7ec3e3183e67574236dfa643f22f39a1fa205c94
u235alfahad <u235alfahad@gmail.com>
2026-03-09 17:04:57 +0600

    added some coments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-
+//initial board representation, negative for black pieces, positive for white pieces, 0 for empty squares
 int board[8][8]={
     {-6,-5,-4,-3,-2,-4,-5,-6},
     {-1,-1,-1,-1,-1,-1,-1,-1},
@@ -11,7 +11,7 @@ int board[8][8]={
     {1,1,1,1,1,1,1,1},
     {6,5,4,3,2,4,5,6},
 };
-
+//function to convert piece representation to character for printing
 void pieceToChar(int x){
     switch (x){
         case 0:
@@ -58,7 +58,7 @@ void pieceToChar(int x){
         break;
     }
 }
-
+//function to print the board
 void board_print(){
     int i,j;
     for(i=0;i<8;i++){
@@ -68,11 +68,11 @@ void board_print(){
     cout << "\n";
     }
 }
-
+//structure to represent a move, with starting and ending coordinates
 struct move{
     int x1,y1,x2,y2;
 };
-
+//main function to test the board printing
 int main()
 {
     board_print();
